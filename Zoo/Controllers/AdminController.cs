16c705cdaaa4782dc549baaf08420f36b109013e
Eliezer Perl <eliezerrules@gmail.com>
2023-06-14 10:26:03 +0300

    added script for liron to add categorys

--- Zoo/Controllers/AdminController.cs
+++ Zoo/Controllers/AdminController.cs
@@ -25,6 +25,14 @@ namespace Zoo.Controllers
         // GET: Admin
         public IActionResult Index()
         {
+            ////FOR LIRON TO ADD CATEGORIES
+            //var cat1 = new Category { Name="Birds" };
+            //var cat2 = new Category { Name="Mammals" };
+            //var cat3 = new Category { Name="Reptiles" };
+            //_categoryService.CreateAsync(cat1);
+            //_categoryService.CreateAsync(cat2);
+            //_categoryService.CreateAsync(cat3);
+
             var animals = _animalService.GetAllAnimalsWithCategories().Result;
 
             return View(animals);
